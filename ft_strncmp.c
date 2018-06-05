4159a40563fcb6f5679a00d9a31573477759e1f8
Alan Barnett <abarnett@e1z3r5p7.42.us.org>
2018-06-05 10:20:23 -0700

    fixing strncmp bug

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: abarnett <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/23 11:04:00 by abarnett          #+#    #+#             */
-/*   Updated: 2018/05/14 20:18:39 by abarnett         ###   ########.fr       */
+/*   Updated: 2018/06/05 10:06:31 by abarnett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,16 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	while (*s1 && *s2 && *s1 == *s2 && n--)
+	while (n - 1 > 0)
 	{
-		s1++;
-		s2++;
+		if (*s1 && *s2 && *s1 == *s2)
+		{
+			++s1;
+			++s2;
+		}
+		else
+			return ((unsigned char)*s1 - (unsigned char)*s2);
+		--n;
 	}
 	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
