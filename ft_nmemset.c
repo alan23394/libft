209ff4748413b20deedc0c05320281b305f8f9f8
Alan Barnett <alanbarnett328@gmail.com>
2019-03-03 00:39:11 -0800

    Move a comment beneath the include line

--- ft_nmemset.c
+++ ft_nmemset.c
@@ -6,16 +6,16 @@
 /*   By: abarnett <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/23 16:35:35 by abarnett          #+#    #+#             */
-/*   Updated: 2018/06/02 17:01:34 by abarnett         ###   ########.fr       */
+/*   Updated: 2019/03/03 00:38:53 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 /*
 ** An implementation of memset that does not segfault if b is NULL
 ** (Returns NULL instead)
 */
-
-#include "libft.h"
 
 void	*ft_nmemset(void *b, int c, size_t len)
 {
