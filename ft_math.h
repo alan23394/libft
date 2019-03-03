4886be96e4b4fc03661f9c31564b6275d097b328
Alan Barnett <alanbarnett328@gmail.com>
2019-03-03 14:14:14 -0800

    Adjust indentation

--- ft_math.h
+++ ft_math.h
@@ -6,27 +6,27 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/03 00:59:54 by alan              #+#    #+#             */
-/*   Updated: 2019/03/03 02:27:02 by alan             ###   ########.fr       */
+/*   Updated: 2019/03/03 02:35:17 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_MATH_H
 # define FT_MATH_H
 
-int					ft_max(int a, int b);
-int					ft_min(int a, int b);
+int		ft_max(int a, int b);
+int		ft_min(int a, int b);
 
-int					ft_vmax(int params, ...);
-int					ft_vmin(int params, ...);
+int		ft_vmax(int params, ...);
+int		ft_vmin(int params, ...);
 
-long				ft_abs(long nb);
+long	ft_abs(long nb);
 
-long				ft_pow(int a, int b);
-double				ft_pow_d(double a, int b);
+long	ft_pow(int a, int b);
+double	ft_pow_d(double a, int b);
 
-int					ft_floor(double roundee);
-int					ft_ceil(double roundee);
+int		ft_floor(double roundee);
+int		ft_ceil(double roundee);
 
-long				ft_round(double roundee);
+long	ft_round(double roundee);
 
 #endif
