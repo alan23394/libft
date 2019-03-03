510de31d4c6c835e63deccbc66c0ec02a8a2a1c8
Alan Barnett <alanbarnett328@gmail.com>
2019-03-03 00:24:09 -0800

    Add lstmemdel

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: abarnett <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/26 10:10:20 by abarnett          #+#    #+#             */
-/*   Updated: 2018/06/01 14:51:45 by abarnett         ###   ########.fr       */
+/*   Updated: 2018/12/14 18:25:54 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,19 @@
 
 void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
 {
-	del((*alst)->content, (*alst)->content_size);
+	if (*alst)
+		del((*alst)->content, (*alst)->content_size);
 	ft_memdel((void **)alst);
+}
+
+/*
+** This function deletes the memory from the void pointer to content. It is
+** meant to be used with lstdel. It ignores the content size, because there's
+** nothing to do with it.
+*/
+
+void	ft_lstmemdel(void *content, size_t content_size)
+{
+	ft_memdel(&content);
+	(void)content_size;
 }
