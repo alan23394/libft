e0534f9df3555a98e9e83cb65b9abc621547cd30
Alan Barnett <alanbarnett328@gmail.com>
2019-03-16 14:02:36 -0700

    Move ft_strndup to new file, change header to ft_string.h

--- ft_strndup.c
+++ ft_strndup.c
@@ -1,23 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_strndup.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abarnett <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/05/14 20:52:19 by abarnett          #+#    #+#             */
-/*   Updated: 2019/03/16 13:39:47 by alan             ###   ########.fr       */
+/*   Created: 2019/03/16 13:39:28 by alan              #+#    #+#             */
+/*   Updated: 2019/03/16 13:39:40 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_string.h"
 
-char	*ft_strdup(const char *s1)
+char	*ft_strndup(const char *s1, size_t len)
 {
 	char	*dup;
-	size_t	len;
 
-	len = ft_strlen(s1);
 	dup = ft_strnew(len);
 	return (dup ? ft_strncpy(dup, s1, len) : 0);
 }
