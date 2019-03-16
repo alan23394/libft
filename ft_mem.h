0836ebebc65cad93da1dfbbe3c128f0ab9b4d07e
Alan Barnett <alanbarnett328@gmail.com>
2019-03-16 01:55:12 -0700

    Add new functions

--- ft_mem.h
+++ ft_mem.h
@@ -6,7 +6,7 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/03 02:47:40 by alan              #+#    #+#             */
-/*   Updated: 2019/03/03 03:56:21 by alan             ###   ########.fr       */
+/*   Updated: 2019/03/05 13:34:50 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,17 @@
 void	*ft_memalloc(size_t size);
 void	ft_memdel(void **ap);
 
+void	ft_bzero(void *s, size_t n);
 void	*ft_memset(void *b, int c, size_t len);
 void	*ft_nmemset(void *b, int c, size_t len);
 
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
+
+void	*ft_memmove(void *dst, const void *src, size_t len);
+
+void	*ft_memchr(const void *s, int c, size_t n);
+
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
 #endif
