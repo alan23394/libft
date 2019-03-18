09652c88ce7194416d9a8053517acf339882721e
Alan Barnett <alanbarnett328@gmail.com>
2019-03-18 12:42:42 -0700

    fix(includes): remove stdlib and stddef from libft

--- libft.h
+++ libft.h
@@ -6,26 +6,12 @@
 /*   By: abarnett <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/17 13:56:25 by abarnett          #+#    #+#             */
-/*   Updated: 2019/03/18 12:28:13 by alan             ###   ########.fr       */
+/*   Updated: 2019/03/18 12:39:20 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
-
-/*
-** TODO remove this include!
-** Some functions still need stdlib in here, but remove it soon!
-*/
-
-# include <stdlib.h>
-
-/*
-** Eventually I should be able to get stddef out of here.
-** It is needed for all the prototypes with size_t.
-*/
-
-# include <stddef.h>
 
 # include "ft_utils.h"
 # include "ft_put.h"
