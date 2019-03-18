468d82df250fb66d72f271d5912e71818028db84
Alan Barnett <alanbarnett328@gmail.com>
2019-03-18 07:43:45 -0700

    chore(comments): remove section comments in ft_string.h

--- ft_string.h
+++ ft_string.h
@@ -6,7 +6,7 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/03 03:13:31 by alan              #+#    #+#             */
-/*   Updated: 2019/03/16 14:10:33 by alan             ###   ########.fr       */
+/*   Updated: 2019/03/18 07:28:00 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,32 +15,15 @@
 
 # include <stddef.h>
 
-/*
-** String allocation/deletion
-*/
-
 char	*ft_strnew(size_t size);
 char	*ft_strinit(size_t size, int c);
 void	ft_strdel(char **as);
 
-/*
-** String copying
-*/
 
 char	*ft_strcpy(char *dst, const char *src);
 char	*ft_strncpy(char *dst, const char *src, size_t len);
 
-/*
-** String duplicating
-*/
-
 char	*ft_strdup(const char *s1);
 char	*ft_strndup(const char *s1, size_t len);
-
-/*
-** General string functions
-*/
-
-size_t	ft_strlen(const char *str);
 
 #endif
