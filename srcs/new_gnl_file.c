c2575be6c07e4ea5a09f9beef89fcf1c21b2186f
Alan Barnett <alanbarnett328@gmail.com>
2019-03-19 00:41:00 -0700

    fix(new_gnl_file): fix name misspelling

--- srcs/new_gnl_file.c
+++ srcs/new_gnl_file.c
@@ -6,14 +6,14 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/18 14:07:05 by alan              #+#    #+#             */
-/*   Updated: 2019/03/18 14:38:27 by alan             ###   ########.fr       */
+/*   Updated: 2019/03/19 00:39:01 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_mem.h"
 #include "ft_gnl_file.h"
 
-t_gnl_file	*new_file(int fd)
+t_gnl_file	*new_gnl_file(int fd)
 {
 	t_gnl_file	*file;
 
