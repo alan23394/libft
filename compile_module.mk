a9def6e96ca2d2b8ad612529129d3e5e5221eb91
Alan Barnett <alanbarnett328@gmail.com>
2019-03-23 06:42:39 -0700

    chore(update): update compile.mk script
    Add a comment, add QUIET checks on the echos, little cleaning

--- compile_module.mk
+++ compile_module.mk
@@ -6,7 +6,7 @@
 #    By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/03/22 23:49:23 by alan              #+#    #+#              #
-#    Updated: 2019/03/23 04:55:13 by alan             ###   ########.fr        #
+#    Updated: 2019/03/23 06:35:56 by alan             ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,25 +15,32 @@
 # C_SRCS should be filled with the src files to compile
 # CC and CFLAGS should be defined somewhere else
 
+# Generate the object and dependency filenames from the srcs
 C_OBJS :=	$(patsubst %.c,%.o,$(C_SRCS))
 DEPENDS :=	$(patsubst %.c,%.d,$(C_SRCS))
 
-.PHONY:		$(NAME)_start $(NAME)_stop $(NAME) clean
+.PHONY:		$(NAME) _start _stop clean
 
-$(NAME): $(NAME)_start $(C_OBJS) $(NAME)_stop
+$(NAME): _start $(C_OBJS) _stop
 
-$(NAME)_start:
-	@echo -ne "$(COMPILE_COLOR)Compiling $(NAME_COLOR)$(NAME) $(DOTS_COLOR)"
+_start:
+	@if [ $(QUIET) -eq 0 ]; then echo -ne\
+		"$(COMPILE_COLOR)Compiling $(NAME_COLOR)$(NAME) $(DOTS_COLOR)";\
+		fi
 
-$(NAME)_stop:
-	@echo -e " $(FINISH_COLOR)done\e[m"
+_stop:
+	@if [ $(QUIET) -eq 0 ]; then echo -e\
+		" $(FINISH_COLOR)done\e[m";\
+		fi
 
 %.o: %.c
 	@$(CC) $(CFLAGS) -MMD -MT $@ -c $< -o $@
-	@echo -n "."
+	@if [ $(QUIET) -eq 0 ]; then echo -n "."; fi
 
 -include $(DEPENDS)
 
 clean:
-	@echo -e "$(DELETE_COLOR)Cleaning $(NAME_COLOR)$(NAME)"
-	@$(RM) $(C_OBJS) $(DEPENDS) name_start name_stop
+	@if [ $(QUIET) -eq 0 ]; then echo -e\
+		"$(DELETE_COLOR)Cleaning $(NAME_COLOR)$(NAME)";\
+		fi
+	@$(RM) $(C_OBJS) $(DEPENDS)
