5fae5ecd41352a83934ae331310075b553098659
Alan Barnett <alanbarnett328@gmail.com>
2019-03-23 06:43:20 -0700

    chore(clean config.mk): align tabs, add comments

--- config.mk
+++ config.mk
@@ -6,29 +6,34 @@
 #    By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/03/23 01:51:49 by alan              #+#    #+#              #
-#    Updated: 2019/03/23 04:18:18 by alan             ###   ########.fr        #
+#    Updated: 2019/03/23 06:39:34 by alan             ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 # Main config file
+# Included in other makefiles
+
+# Set this variable to 0 to enable printing
+# Set this variable to 1 to disable printing
+QUIET :=			0
 
 # Base src directory
-SRC_DIR :=	./srcs
+SRC_DIR :=			./srcs
 # Directories to include
-INCLUDE :=	./includes
+INCLUDE :=			./includes
 # Location of makefile modules
-MODULES_DIR :=	./modules
+MODULES_DIR :=		./modules
 
 # Add a -I on each INCLUDE
-INCLUDE :=	$(foreach DIR, $(INCLUDE), -I$(DIR))
+INCLUDE :=			$(foreach DIR, $(INCLUDE), -I$(DIR))
 
 # Compiler and flags
-CC :=		gcc
-CFLAGS +=	-g -Wall -Wextra -Werror $(INCLUDE)
+CC :=				gcc
+CFLAGS +=			-g -Wall -Wextra -Werror $(INCLUDE)
 
 # Colors
 COMPILE_COLOR :=	\e[1;32m
 NAME_COLOR :=		\e[1;33m
-DOTS_COLOR :=		\e[1;32m
+DOTS_COLOR :=		\e[1;35m
 FINISH_COLOR :=		\e[0;32m
 DELETE_COLOR :=		\e[1;31m
