7575a68cc64c2fc78bad36ec8dbb4a682f35993c
Alan Barnett <alanbarnett328@gmail.com>
2019-04-09 03:23:12 -0700

    fix(word_count): use wordnext to jump to next word
    This function used to count a delimeter, but using wordnext to jump over
    characters and spaces to the next character is a lot easier, and also
    better, because it skips space properly

--- srcs/ft_word/ft_wordcount.c
+++ srcs/ft_word/ft_wordcount.c
@@ -1,30 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_count_words.c                                   :+:      :+:    :+:   */
+/*   ft_wordcount.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abarnett <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/26 10:37:07 by abarnett          #+#    #+#             */
-/*   Updated: 2019/03/18 12:36:21 by alan             ###   ########.fr       */
+/*   Updated: 2019/04/07 22:50:36 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_count_words(const char *str, char del)
+#include "ft_word.h"
+
+int	ft_wordcount(const char *str)
 {
 	int count;
 
 	count = 0;
 	while (*str)
 	{
-		if (*str != del)
-		{
-			count++;
-			while (*str && *str != del)
-				str++;
-		}
-		else
-			str++;
+		str = ft_wordnext(str);
+		++count;
 	}
 	return (count);
 }
