c6288941f1f718a854a3353b07dc0e9c883355af
Alan Barnett <alanbarnett328@gmail.com>
2019-04-22 05:25:23 -0700

    fix(list): check del function
    Check if I pass in a del function pointer for lstdelone. This is
    convenient for when I don't want to delete the content pointer.

--- srcs/ft_list/ft_lstdelone.c
+++ srcs/ft_list/ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: abarnett <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/26 10:10:20 by abarnett          #+#    #+#             */
-/*   Updated: 2019/03/03 03:45:27 by alan             ###   ########.fr       */
+/*   Updated: 2019/04/17 16:30:01 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
 {
-	if (*alst && (*alst)->content)
+	if (*alst && (*alst)->content && del)
 		del((*alst)->content, (*alst)->content_size);
 	ft_memdel((void **)alst);
 }
