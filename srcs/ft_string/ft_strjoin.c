1e4515a6d9343e54f2d05c037044feb6bf1ffb68
Alan Barnett <alanbarnett328@gmail.com>
2019-06-16 02:26:50 -0700

    chore(strjoin): cache string lengths
    Switch to strncpy instead of strcpy and strcat, so that the lengths of
    the strings can only be calculated once.

--- srcs/ft_string/ft_strjoin.c
+++ srcs/ft_string/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: abarnett <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/22 20:55:44 by abarnett          #+#    #+#             */
-/*   Updated: 2019/03/18 09:25:53 by alan             ###   ########.fr       */
+/*   Updated: 2019/06/15 23:53:06 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,19 @@
 
 char	*ft_strjoin(const char *s1, const char *s2)
 {
+	int		s1len;
+	int		s2len;
 	char	*ret;
 
 	if (!s1 || !s2)
 		return (0);
-	ret = ft_strnew(ft_strlen(s1) + ft_strlen(s2));
+	s1len = ft_strlen(s1);
+	s2len = ft_strlen(s2);
+	ret = ft_strnew(s1len + s2len);
 	if (ret)
 	{
-		ft_strcpy(ret, s1);
-		ft_strcat(ret, s2);
+		ft_strncpy(ret, s1, s1len);
+		ft_strncpy(ret + s1len, s2, s2len);
 	}
 	return (ret);
 }
