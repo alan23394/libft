5293250da4bf9f17fcadba48404eba3653b1a8ad
Alan Barnett <alanbarnett328@gmail.com>
2019-11-11 14:01:52 -0800

    update(node): add size_t size parameter

--- includes/ft_node.h
+++ includes/ft_node.h
@@ -6,19 +6,23 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/19 17:28:59 by alan              #+#    #+#             */
-/*   Updated: 2019/10/19 18:10:15 by alan             ###   ########.fr       */
+/*   Updated: 2019/10/25 15:35:19 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_NODE_H
 # define FT_NODE_H
 
+# include <stddef.h>
+
 struct	s_node
 {
 	void			*content;
+	size_t			content_size;
 	struct s_node	*next;
 };
 
-struct s_node	*ft_node_init(void *content, struct s_node *next);
+struct s_node	*ft_node_init(void *content, size_t content_size,
+					struct s_node *next);
 
 #endif
--- srcs/ft_node/ft_node_init.c
+++ srcs/ft_node/ft_node_init.c
@@ -6,14 +6,16 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/19 17:30:14 by alan              #+#    #+#             */
-/*   Updated: 2019/10/19 17:49:46 by alan             ###   ########.fr       */
+/*   Updated: 2019/10/25 15:36:01 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_node.h"
 #include "ft_mem.h"
+#include <stddef.h>
 
-struct s_node	*ft_node_init(void *content, struct s_node *next)
+struct s_node	*ft_node_init(void *content, size_t content_size,
+					struct s_node *next)
 {
 	struct s_node	*new_node;
 
@@ -21,6 +23,7 @@ struct s_node	*ft_node_init(void *content, struct s_node *next)
 	if (!new_node)
 		return (0);
 	new_node->content = content;
+	new_node->content_size = content_size;
 	new_node->next = next;
 	return (new_node);
 }
