c38fb092bae740a8be0fb67471bf3f949af0c6a1
Alan Barnett <alanbarnett328@gmail.com>
2020-04-14 18:04:42 -0700

    fix(wordcount): skip leading whitespace

--- srcs/ft_word/ft_wordcount.c
+++ srcs/ft_word/ft_wordcount.c
@@ -6,17 +6,24 @@
 /*   By: abarnett <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/26 10:37:07 by abarnett          #+#    #+#             */
-/*   Updated: 2019/04/07 22:50:36 by alan             ###   ########.fr       */
+/*   Updated: 2020/03/18 03:20:11 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_word.h"
+#include "ft_utils.h"
+
+/*
+** Counts the amount of words in a string
+** Skips leading whitespace
+*/
 
 int	ft_wordcount(const char *str)
 {
 	int count;
 
 	count = 0;
+	str = ft_skipspace(str);
 	while (*str)
 	{
 		str = ft_wordnext(str);
