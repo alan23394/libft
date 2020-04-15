b7a33f67aad33dc436ff8bef76320f63665c8422
Alan Barnett <alanbarnett328@gmail.com>
2020-04-14 18:04:22 -0700

    chore(comment): clarify wordnext

--- srcs/ft_word/ft_wordnext.c
+++ srcs/ft_word/ft_wordnext.c
@@ -6,16 +6,17 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/07 22:36:04 by alan              #+#    #+#             */
-/*   Updated: 2019/04/07 22:50:27 by alan             ###   ########.fr       */
+/*   Updated: 2020/03/18 03:18:41 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_utils.h"
 
 /*
-** This function skips all non-whitespace characters (i.e. the current word),
-** and then skips all whitespace characters (up to the next word). It stops at
-** null terminators also.
+** This function returns a pointer to the start of the next word in a string.
+** It skips all non-whitespace characters (i.e. the current word), and then
+** skips all whitespace characters (up to the next word). It also stops at null
+** terminators.
 */
 
 const char	*ft_wordnext(const char *str)
